94f10a4f58ff43c9160d76417492f6bf33b127c0
Jakaria Mashud Shahria <jakariamsria@gmail.com>
2021-06-13 13:05:30 +0600

    add meta in index.js head

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,9 @@ export default function Home() {
                 <title>Basa Bari | FIND YOUR HOME - Across the Cities.</title>
                 <meta name="description" content="Basa Bari is an Android and iOS Application for finding rental home across cities in Bangladesh." />
                 <link rel="icon" href="/favicon.ico" />
+                <meta property="og:title" content={'Basa Bari | FIND YOUR HOME - Across the Cities.'}/>
+                <meta property="og:description" content={'Basa Bari is an Android and iOS Application for finding rental home across cities in Bangladesh.'}/>
+                <meta property="og:image" content={'/assets/app-image-png'}/>
             </Head>
             <Intro/>
             <Features/>
